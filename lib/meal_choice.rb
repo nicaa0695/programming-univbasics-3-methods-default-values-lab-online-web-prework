e90b689cc90fd4d8fdb5ad96a73382177dc50ba5
Veronica Lopez <nicaa0695@gmail.com>
2019-06-30 20:55:29 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,13 @@
 # Your code here
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  veg1 = "A plate of meat with broccoli and macaroni."
+  veg2 = "A plate of tofu with broccoli and macaroni."
+end
+class meal
+  attr_reader :choice
+def initialize(choice = "meat")
+  @choice = choice
+end

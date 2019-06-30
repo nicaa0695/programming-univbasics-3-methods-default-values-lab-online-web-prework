f86b468162c8ed9826922034b42fe298e0e53f7d
Veronica Lopez <nicaa0695@gmail.com>
2019-06-30 20:53:36 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,7 +6,5 @@ def meal_choice(veg1, veg2, protein = "meat")
   veg1 = "A plate of meat with broccoli and macaroni."
   veg2 = "A plate of tofu with broccoli and macaroni."
 end
-def meal_choice(veg1,veg2)
-  return protein
-end
+
 

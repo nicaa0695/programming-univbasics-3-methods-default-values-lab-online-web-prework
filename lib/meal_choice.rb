2b19a1e5ad5e83c5000f653d44183c0b7939edd7
Veronica Lopez <nicaa0695@gmail.com>
2019-06-30 20:55:14 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,5 +6,8 @@ def meal_choice(veg1, veg2, protein = "meat")
   veg1 = "A plate of meat with broccoli and macaroni."
   veg2 = "A plate of tofu with broccoli and macaroni."
 end
-
-
+class meal
+  attr_reader :choice
+def initialize(choice = "meat")
+  @choice = choice
+end
